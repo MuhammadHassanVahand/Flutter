ab0d640f7c3e6a086da6218674a7cd82a510e3b7
MuhammadHassanVahand <hassan12rehan@gmail.com>
2023-07-29 22:56:32 +0500

    Constructor Practice

--- oops dart/opps practice.dart
+++ oops dart/opps practice.dart
@@ -1,15 +1,29 @@
-class ClassName {
-  // Instance Variables
-  int? number;
+class Number {
   String? name;
-  // Parameterized Constructor
-  ClassName(this.number, this.name) {
-    print("Contructor");
+  int? num1;
+  double? num2;
+
+  Number(String name, int num1, double num2) {
+    this.name = name;
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  Number.another(String name, int num1, double num2) {
+    this.name = name;
+    this.num1 = num1;
+    this.num2 = num2;
   }
 }
 
 void main() {
-  ClassName className = ClassName(3, "Hassan");
-  print("${className.name}");
-  print("${className.number}");
+  Number number = Number("Hassn", 34, 7.5);
+  print(number.name);
+  print(number.num1);
+  print(number.num2);
+  print("");
+
+  Number number2 = Number.another("Shaka", 54, 5446.588);
+  print(number2.name);
+  print(number2.num1);
+  print(number2.num2);
 }
